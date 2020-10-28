01e978624dfcc9845fd3189178e2535c31185b84
jeremyv83 <jeremyvivie@hotmail.fr>
2020-10-28 16:17:29 +0100

    ajout du js pour les fruits

---
+++ javascript/exobidon/script.js
@@ -0,0 +1,19 @@
+var fraise = ["fraise", "rouge", "rond"],
+    melon = ["melon", "orange", "rond"],
+    banane = ["banane", "jaune", "croissant"],
+    pomme = ["pomme", "vert", "rond"],
+    orange = ["orange", "orange", "rond"];
+var fruits = {
+    fraise,
+    melon,
+    banane,
+    pomme,
+    orange
+}
+
+
+console.log(fruits);
+   
+    
+
+
